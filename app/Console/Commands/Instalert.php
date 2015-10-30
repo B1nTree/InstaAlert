c5315747f08320356f260250180c76f28e29185a
Dylan Pierce <me@dylanjpierce.com>
2015-10-29 22:01:52 -0400

    Ding ding ding! You're the winner Dom

--- app/Console/Commands/Instalert.php
+++ app/Console/Commands/Instalert.php
@@ -89,7 +89,7 @@ class Instalert extends Command {
 				continue;
 			}
 
-			$numbers = array('330-730-9623', '609-410-8626');
+			$numbers = array('267-229-3042');
 
 			$post_time = Carbon::createFromTimeStamp($post->created_time);
 			$now = Carbon::now();
